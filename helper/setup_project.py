d5ca66f72cafdc7884b9cb7a46b37f062b61ac20
Michelle <miegger@student.ethz.ch>
2025-08-26 10:37:32 +0200

    added a few new clicking functions

--- helper/setup_project.py
+++ helper/setup_project.py
@@ -21,6 +21,10 @@ def clicking_function_combined(p, opinions, theta=0.5*np.ones(5)):
   """Clicking function model: 0.5(1-theta) + theta * exp(-4 * (opinions - p)**2)"""
   return 0.5*(np.ones_like(p) - theta) + theta*np.exp(-4*(opinions - p)**2)
 
+def clicking_function_squared_combined(p, opinions, theta=0.5*np.ones(5)):
+  """Clicking function model: 0.5(1-theta) + theta * (1 - 0.25(opinions - p)**2)"""
+  return 0.5*(np.ones_like(p) - theta) + theta*(np.ones_like(p) - 0.25*(opinions - p)**2)
+
 def kalman_filter(delta_p, delta_cr, true_sensitivity, N):
     sigma_r = 0.1 # Std of measurement noise --> low values we trust fully the CTR
     sigma_q = 0.1 # Std of process noise
@@ -50,7 +54,7 @@ def kalman_filter(delta_p, delta_cr, true_sensitivity, N):
     return l.reshape((N, N), order='F')
 
 
-def generate_model(num_measurements, ideal=True, clicking_function=['squared', 'exponential', 'exponential_half', 'linear', 'combined']):
+def generate_model(num_measurements, ideal=True, clicking_function=['squared', 'exponential', 'exponential_half', 'linear', 'combined', 'squared combined']):
   num_samples = num_measurements
   d = 5  # Dimension of p and x
   A = np.array([[0.15, 0.15, 0.1, 0.2, 0.4],[0, 0.55, 0, 0, 0.45],[0.3, 0.05, 0.05, 0, 0.6],[0, 0.4, 0.1, 0.5, 0],[0, 0.3, 0, 0, 0.7]])
@@ -63,7 +67,7 @@ def generate_model(num_measurements, ideal=True, clicking_function=['squared', '
   theta = np.random.normal(0.25, 0.1, d)
   if clicking_function == 'exponential': 
     theta = np.random.normal(1, 0.25, d)
-  if clicking_function == 'combined': 
+  if clicking_function == 'combined' or clicking_function == 'squared combined' or clicking_function == 'mixed': 
     theta = np.random.uniform(0, 1, d)
   
   P = np.random.uniform(low=-1, high=1, size=(num_samples, d))
@@ -97,6 +101,12 @@ def generate_model(num_measurements, ideal=True, clicking_function=['squared', '
         CTR_obs[i] = clicking_function_exponential_half(P[i], X[i])
       elif clicking_function == 'combined':
         CTR_obs[i] = clicking_function_combined(P[i], X[i], theta=theta)
+      elif clicking_function == 'squared combined':
+        CTR_obs[i] = clicking_function_squared_combined(P[i], X[i], theta=theta)
+      elif clicking_function == 'mixed':
+        CTR_obs[i,1:3] = clicking_function_squared_combined(P[i,1:3], X[i,1:3], theta=theta[1:3])
+        CTR_obs[i,0] = clicking_function_combined(P[i,0], X[i,0], theta=theta[0])
+        CTR_obs[i,3:] = clicking_function_combined(P[i,3:], X[i,3:], theta=theta[3:])
 
     return (sim, P, CTR_obs, theta, sensitivity)
   
